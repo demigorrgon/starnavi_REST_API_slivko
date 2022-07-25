de815fd14eecb6c67a52f19db34788c3d03e6133
Valentyn Slivko <metallistvalon@gmail.com>
2022-07-25 15:15:23 +0300

    Add BaseUser, Post models to admin site

--- posts/admin.py
+++ posts/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from posts.models import Post
 
-# Register your models here.
+# admin.site.register(Post)
+
+
+@admin.register(Post)
+class PostAdmin(admin.ModelAdmin):
+    # explicit column specification redundant in this case,
+    # but comes in handy if we have a lot of columns that could slow down admin site
+    list_display = ("id", "title", "creator", "content", "created_at", "updated_at")
--- users/admin.py
+++ users/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from users.models import BaseUser
 
-# Register your models here.
+
+@admin.register(BaseUser)
+class UserAdmin(admin.ModelAdmin):
+    # explicit column specification redundant in this case,
+    # but comes in handy if we have a lot of columns that could slow down admin site
+    list_display = ("id", "username", "email", "first_name", "last_name")
