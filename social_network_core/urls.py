3ba443d9f1aef68ca15154e6431f95e7b3568357
Valentyn Slivko <metallistvalon@gmail.com>
2022-07-25 20:18:19 +0300

    Add JWT token related endpoints

--- social_network_core/urls.py
+++ social_network_core/urls.py
@@ -15,9 +15,17 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import include, path
+from rest_framework_simplejwt.views import (
+    TokenObtainPairView,
+    TokenRefreshView,
+    TokenVerifyView,
+)
 
 urlpatterns = [
     path("admin/", admin.site.urls),
+    path("api/token/", TokenObtainPairView.as_view(), name="obtain-token"),
+    path("api/token/refresh/", TokenRefreshView.as_view(), name="refresh-token"),
+    path("api/token/verify/", TokenVerifyView.as_view(), name="verify-token"),
     path("api/posts/", include("posts.urls")),
     path("api/users/", include("users.urls")),
 ]
