d666d6aa1bedb65418ba978217d9eab432baad59
Valentyn Slivko <metallistvalon@gmail.com>
2022-07-25 20:17:11 +0300

    Register Like model in admin

--- posts/admin.py
+++ posts/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from posts.models import Post
+from posts.models import Post, Like
 
 # admin.site.register(Post)
 
@@ -7,5 +7,10 @@ from posts.models import Post
 @admin.register(Post)
 class PostAdmin(admin.ModelAdmin):
     # explicit column specification redundant in this case,
-    # but comes in handy if we have a lot of columns that could slow down admin site
+    # but comes in handy if we have a lot of columns that could lag admin site
     list_display = ("id", "title", "creator", "content", "created_at", "updated_at")
+
+
+@admin.register(Like)
+class LikeAdmin(admin.ModelAdmin):
+    list_display = ("id", "liked_post", "liked_by", "liked_at")
