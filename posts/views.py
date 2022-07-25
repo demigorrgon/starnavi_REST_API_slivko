7d1d78bc9424942d1c284cfef19cf30e2233b2b7
Valentyn Slivko <metallistvalon@gmail.com>
2022-07-25 20:49:35 +0300

    Remove redundant imports Moved AnalyticsViewSet to its own analytics folder

--- posts/views.py
+++ posts/views.py
@@ -1,11 +1,10 @@
-from datetime import datetime
 from django.db import IntegrityError
 from rest_framework.viewsets import ModelViewSet
 from rest_framework.response import Response
 from rest_framework import status
 from rest_framework.decorators import action
 from rest_framework.permissions import AllowAny
-from posts.models import Like, Post
+from posts.models import Post
 from posts.serializers import PostSerializer
 
 
@@ -67,44 +66,3 @@ class PostViewset(ModelViewSet):
             )
         self.perform_destroy(obj_to_delete)
         return Response(status=status.HTTP_204_NO_CONTENT)
-
-
-class LikeAnalyticsViewSet(ModelViewSet):
-    http_method_names = ["get"]
-
-    def get_queryset(self):
-        # django-filter prob would be better
-        date_from = self.request.query_params.get("date_from")
-        date_to = self.request.query_params.get("date_to")
-        if date_from and date_to:
-            likes_between_provided_dates = Like.objects.filter(
-                created_at__date__range=(
-                    datetime.strptime(date_from, "%Y-%M-%d"),
-                    datetime.strptime(date_to, "%Y-%M-%d"),
-                )
-            )
-            print(likes_between_provided_dates)
-            return likes_between_provided_dates
-        # return Response(
-        #     data={
-        #         "response": "Please provide 'date_from' and 'date_to' query parameters (in url)"
-        #     },
-        #     status=status.HTTP_400_BAD_REQUEST,
-        # )
-
-    def detail(self, request, *args, **kwargs):
-        likes_in_date_range = self.get_queryset()
-        if not likes_in_date_range:
-            return Response(
-                data={
-                    "response": "There are no likes in that time period, are provided dates correct?"
-                },
-                status=status.HTTP_200_OK,
-            )
-
-        return Response(
-            data={
-                "response": f"There was {likes_in_date_range.count()} likes in that timespan"
-            },
-            status=status.HTTP_200_OK,
-        )
