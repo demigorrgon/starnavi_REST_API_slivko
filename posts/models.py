45fb07ffec97b85ce74d24b1148b7823f1d3b7b4
Valentyn Slivko <metallistvalon@gmail.com>
2022-07-25 18:05:05 +0300

    Add Like model

--- posts/models.py
+++ posts/models.py
@@ -8,6 +8,18 @@ class Post(models.Model):
     content = models.CharField(max_length=1000)
     created_at = models.DateTimeField(auto_now=True)
     updated_at = models.DateTimeField(auto_now_add=True)
+    likes = models.ManyToManyField(
+        BaseUser, related_name="likes", blank=True, through="Like"
+    )
 
     def __str__(self) -> str:
         return f"id: {self.pk}, title: {self.title} created by: {self.creator.username}"
+
+
+class Like(models.Model):
+    liked_post = models.ForeignKey(Post, on_delete=models.CASCADE)
+    liked_by = models.ForeignKey(BaseUser, on_delete=models.CASCADE)
+    liked_at = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        unique_together = [["liked_post", "liked_by"]]
