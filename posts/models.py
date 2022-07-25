c439b0fd8555241e215ad4f3f940b1e8446f4416
Valentyn Slivko <metallistvalon@gmail.com>
2022-07-25 15:04:23 +0300

    Create Post model

--- posts/models.py
+++ posts/models.py
@@ -1,3 +1,13 @@
 from django.db import models
+from users.models import BaseUser
 
-# Create your models here.
+
+class Post(models.Model):
+    title = models.CharField(max_length=128)
+    creator = models.ForeignKey(BaseUser, on_delete=models.CASCADE)
+    content = models.CharField(max_length=1000)
+    created_at = models.DateTimeField(auto_now=True)
+    updated_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self) -> str:
+        return f"id: {self.pk}, title: {self.title} created by: {self.creator.username}"
