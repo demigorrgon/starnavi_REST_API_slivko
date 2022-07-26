0389b761cda80a82308d3146fa3bed8dfe958f7b
Valentyn Slivko <metallistvalon@gmail.com>
2022-07-26 08:44:22 +0300

    Add new user model fields to serializer Add serializer for user's activity view

--- users/serializers.py
+++ users/serializers.py
@@ -7,5 +7,20 @@ class UserSerializer(ModelSerializer):
     posts = PostSerializer(read_only=True, many=True)
 
     class Meta:
-        fields = ("id", "username", "email", "first_name", "last_name", "posts")
+        fields = (
+            "id",
+            "username",
+            "email",
+            "first_name",
+            "last_name",
+            "last_login",
+            "last_activity",
+            "posts",
+        )
+        model = BaseUser
+
+
+class UserActivitySerializer(UserSerializer):
+    class Meta:
+        fields = ("username", "last_login", "last_activity")
         model = BaseUser
