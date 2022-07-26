d7ff1b9b3c1a345fd32d9a75cf74353e63961b13
Valentyn Slivko <metallistvalon@gmail.com>
2022-07-26 09:00:47 +0300

    Add explicit True to hidden debug value

--- social_network_core/settings.py
+++ social_network_core/settings.py
@@ -11,7 +11,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.getenv("SECRET_KEY")
 
 if os.environ.get("GITHUB_WORKFLOW"):
-    DEBUG = int(os.environ.get("DEBUG"))
+    DEBUG = True
 else:
     DEBUG = int(os.getenv("DEBUG"))
 
