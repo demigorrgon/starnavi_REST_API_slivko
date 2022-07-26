b3c39c6c2455f8124a5d2e23994cb47253dd7bca
Valentyn Slivko <metallistvalon@gmail.com>
2022-07-26 08:43:04 +0300

    Protect 'like' and 'dislike' actions with IsAuthenticated permission

--- posts/views.py
+++ posts/views.py
@@ -1,9 +1,9 @@
 from django.db import IntegrityError
 from rest_framework.viewsets import ModelViewSet
 from rest_framework.response import Response
+from rest_framework.permissions import IsAuthenticated
 from rest_framework import status
 from rest_framework.decorators import action
-from rest_framework.permissions import AllowAny
 from posts.models import Post
 from posts.serializers import PostSerializer
 
@@ -11,9 +11,15 @@ from posts.serializers import PostSerializer
 class PostViewset(ModelViewSet):
     queryset = Post.objects.all()
     serializer_class = PostSerializer
+    permission_classes = [IsAuthenticated]
     http_method_names = ["get", "post", "delete"]
 
-    @action(detail=True, methods=["POST"], permission_classes=[AllowAny])
+    @action(
+        detail=True,
+        methods=["POST"],
+        permission_classes=[IsAuthenticated],
+        url_name="like",
+    )
     def like(self, request, *args, **kwargs):
         post = self.get_object()
         post.likes.add(request.user)
@@ -23,7 +29,12 @@ class PostViewset(ModelViewSet):
             data={"response": "Post was liked successfully"}, status=status.HTTP_200_OK
         )
 
-    @action(detail=True, methods=["POST"], permission_classes=[AllowAny])
+    @action(
+        detail=True,
+        methods=["POST"],
+        permission_classes=[IsAuthenticated],
+        url_name="dislike",
+    )
     def dislike(self, request, *args, **kwargs):
         post = self.get_object()
         post.likes.remove(request.user)
@@ -35,12 +46,6 @@ class PostViewset(ModelViewSet):
         )
 
     def create(self, request, *args, **kwargs):
-        if not request.user.is_authenticated:
-            return Response(
-                data={"response": "Authorization token required"},
-                status=status.HTTP_403_FORBIDDEN,
-            )
-
         data = request.data
         creator = request.user
         try:
@@ -61,7 +66,7 @@ class PostViewset(ModelViewSet):
         obj_to_delete = self.get_object()
         if request.user.username != obj_to_delete.creator.username:
             return Response(
-                data={"response": "You cannot delete someone else's "},
+                data={"response": "You cannot delete someone else's post"},
                 status=status.HTTP_403_FORBIDDEN,
             )
         self.perform_destroy(obj_to_delete)
